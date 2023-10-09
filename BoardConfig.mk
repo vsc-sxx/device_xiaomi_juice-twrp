5ab3c1731b138f31515924b11a6938b1cf28cf7b
redznn <h908659@gmail.com>
2023-10-09 17:15:57 -0300

    spes: Use LZ4 compression for ramdisks
    Change-Id: Ia355a543caccd0e2ea0278477725442ce34d5da9
    Signed-off-by: redznn <h908659@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -92,6 +92,7 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CONFIG := vendor/spes-perf_defconfig
 TARGET_KERNEL_HEADERS := kernel/xiaomi/spes
 TARGET_KERNEL_SOURCE := kernel/xiaomi/spes
+BOARD_RAMDISK_USE_LZ4 := true
 
 # Metadata
 BOARD_BUILD_SYSTEM_ROOT_IMAGE := false
